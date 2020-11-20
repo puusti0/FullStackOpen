9e2eccf0fef20944d49773dc6704e76d7d324663
Janne Valle <janne.valle@metropolia.fi>
2020-11-20 12:27:19 +0200

    Reenailua

--- osa1/harkka1/src/index.js
+++ osa1/harkka1/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
